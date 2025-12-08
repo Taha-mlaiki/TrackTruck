c15c9901c867994a89dc3bfc333e71546005c2df
taha <mlaikitaha29@gmail.com>
2025-12-08 16:05:11 +0100

    TRAC-11 Create the auth controller

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,18 +1,25 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
-
-const app = express();
-app.use(express.json());
+import mongoose from "mongoose";
 
 // Load environment variables from .env file
 dotenv.config();
 
+const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req:Request, res:Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
+  try {
+    await mongoose.connect(process.env.DATABASE_URL || "");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
 });
