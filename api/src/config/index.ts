d33880f183de715fd648ff6e09a9a9647c8c6b5b
taha <mlaikitaha29@gmail.com>
2025-12-10 09:50:43 +0100

    TRAC-20 : Create the logger and update the the mongoose connection

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -3,9 +3,11 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
-  MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/fleetdb",
+  MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/TrackTruck",
   JWT_SECRET: process.env.JWT_SECRET || "change_me",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1h",
   COOKIE_SECURE: process.env.COOKIE_SECURE === "true",
-  NODE_ENV: process.env.NODE_ENV || "development"
+  NODE_ENV: process.env.NODE_ENV || "development",
+  SEED_ADMIN_EMAIL: process.env.SEED_ADMIN_EMAIL || "admin@example.com",
+  SEED_ADMIN_PASSWORD: process.env.SEED_ADMIN_PASSWORD || "Admin123!",
 };
