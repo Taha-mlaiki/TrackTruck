7b70c3e50e0b7a567756c1752843989f2083e9d3
taha <mlaikitaha29@gmail.com>
2025-12-10 10:23:14 +0100

    TRAC-22 create the auth middleware with the validation middlware using zode

---
+++ api/src/middlewares/auth.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../cors/errors/AppError";
+import config from "../config";
+import jwt from "jsonwebtoken";
+export interface AuthRequest extends Request {
+  user?: { id: string; role: string; email?: string };
+}
+
+export const requireAuth = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return next(new AppError(401, "Authentication required"));
+  }
+  try {
+    const payload = jwt.verify(token, config.JWT_SECRET) as any;
+    req.user = {
+      id: String(payload.sub),
+      role: payload.role,
+      email: payload.email,
+    };
+    next();
+  } catch (error) {
+    return next(new AppError(401, "Invalid token"));
+  }
+};
---
+++ api/src/middlewares/validation.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodSchema } from "zod";
+import { AppError } from "../cors/errors/AppError";
+
+export const validate = (schema: ZodSchema<any>) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error: any) {
+      return next(new AppError(400, "validation failed", error.errors));
+    }
+  };
+};
